f0976d069bc7ecc4d22214878dbc89d9979879b7
Umar Shakar Mac <max_umarshakar@gmail.com>
2025-11-25 11:18:00 +0400

    Fixing hero video for mobile

--- assets/theme.js
+++ assets/theme.js
@@ -36,5 +36,15 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  // Set viewport height unit for mobile
+  const setVh = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  };
+
+  window.addEventListener('resize', setVh);
+  window.addEventListener('orientationchange', setVh);
+  setVh();
 });
 
